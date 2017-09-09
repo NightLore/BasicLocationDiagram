75db7be408d37b98bdf8fc5098e1c1ccdfb0681a
NightLore <nathanlui2010@gmail.com>
2017-09-09 14:18:28 -0700

    Clean up, added auto hightlight input, renamed vars

--- diagram.js
+++ diagram.js
@@ -1,4 +1,29 @@
 
+var points = [];
+var ENTER = 13;
+var selected = false;
+// --------------------------------------------------------------- //
+window.onload = function() {
+    setupVariables('diagram_canvas');
+    setupInput(document);
+//    setupInput(canvas);
+
+    // ------------------- initialize right overlay ------------------- //
+    var d1 = document.createElement('div');
+    var s1 = createPointIntegerInput(origin,'span','x','x','20%',true,true);
+    var s2 = createPointIntegerInput(origin,'span','y','y','20%',true,true);
+
+    var s3 = document.createElement('span');
+    s3.appendChild(document.createTextNode('....'));
+    s3.style.visibility = "hidden";
+
+    d1.appendChild(s1);
+    d1.appendChild(s3);
+    d1.appendChild(s2);
+
+    document.getElementById('origin').appendChild(d1);
+}
+
 var setupVariables = function(c) {
     mouse = {
         x: 0,
@@ -95,4 +120,117 @@ var draw = function() {
     for (var i = 0; i < points.length; i++) {
         points[i].draw(context2D,points);
     }
+}
+
+// ----------------------------- HTML Element Functions -------------------------------- //
+
+function resetBackgroundField(textfield) {
+    textfield.value = document.getElementById('overlay').style.backgroundColor;
+}
+
+function setBackground(textfield, element) {
+    if (event.keyCode == ENTER) {
+        element.style.backgroundColor = textfield.value;
+    }
+}
+
+function addPoint() {
+    var point_num = points.length;
+
+    var p = new Sprite(origin.x, origin.y);
+    p.index = point_num;
+    p.text = "Point " + (point_num + 1);
+    p.handler[0] = function() {
+        selected = (selected === p) ? false : p;
+    }
+    points.push(p);
+
+    var title = document.createTextNode('Point ' + (point_num + 1) + ': ');
+
+    var coords_div = document.createElement('div');
+    var x_span = createPointIntegerInput(p,'span','x','x','20%',true,true);
+    var y_span = createPointIntegerInput(p,'span','y','y','20%',true,true);
+
+    var space = document.createElement('span');
+    space.appendChild(document.createTextNode('....'));
+    space.style.visibility = "hidden";
+
+    coords_div.appendChild(x_span);
+    coords_div.appendChild(space);
+    coords_div.appendChild(y_span);
+    
+    var radius_div = createPointIntegerInput(p,'div','Radius','radius','40%',true,true);
+    var color_div = createPointInput(p,'div','Set Color','fillColor','40%',true);
+
+    var pointDiv = document.createElement('div');
+    pointDiv.appendChild(title);
+    pointDiv.appendChild(coords_div);
+    pointDiv.appendChild(radius_div);
+    pointDiv.appendChild(color_div);
+    pointDiv.appendChild(document.createElement('br'));
+    document.getElementById('points').appendChild(pointDiv);
+
+    update();
+}
+
+function createPointInput(p, container, text, value, width, keyup, blur) {
+    var c = document.createElement(container);
+    var t = document.createTextNode(text + ': ');
+    var i = document.createElement('input');
+    i.style.width = width;
+    i.value = p[value];
+    i.addEventListener('click', function(e) {this.select()});
+    if (keyup) {
+        if (typeof keyup != 'function') {
+            keyup = function(e) {
+                if (e.keyCode == ENTER) {
+                    p[value] = i.value;
+                }
+            };
+        }
+        i.addEventListener('keyup', keyup);
+    }
+    if (blur) {
+        if (typeof blur != 'function') {
+            blur = function(e) {
+                p[value] = i.value;
+                update();
+            }
+        }
+        i.addEventListener('blur', blur);
+    }
+    c.appendChild(t);
+    c.appendChild(i);
+    return c;
+}
+
+function createPointIntegerInput(p,container, text, value, width, keyup, blur) {
+    var c = createPointInput(p, container, text, value, width);
+    var i = c.getElementsByTagName('input')[0];
+    if (keyup) {
+        if (typeof keyup != 'function') {
+            keyup = function(e) {
+                if (e.keyCode == ENTER) {
+                    if (isInt(i.value)) {
+                        p[value] = parseFloat(i.value);
+                        update();
+                    }
+                }
+            };
+        }
+        i.addEventListener('keyup', keyup);
+    }
+    if (blur) {
+        if (typeof blur != 'function') {
+            blur = function(e) {
+                if (isInt(i.value)) 
+                    p[value] = parseFloat(i.value);
+                else
+                    i.value = p[value];
+                update();
+            }
+        }
+        i.addEventListener('blur', blur);
+    }
+    return c;
 }
